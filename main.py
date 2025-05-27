d0a945ed0f6ad03c1098bac17c5010ed8759c776
unknown <nickmarsh54@gmail.com>
2025-05-27 18:42:28 -0500

    github user test

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import argparse
+import requests
+ 
+def check_github(username):
+	url = f"https://github.com/{username}"
+	try:
+	   response = requests.get(url, timeout=5)
+	   return response.status_code == 200
+	except requests.RequestException:
+	   return False
+ 
 def main():
-    print("OSINT Profile Recon Tool")
-
-if __name__ == "__main__":
-    main()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--username", help="Username to search on Github")
+	args = parser.parse_args()
+ 
+	if args.username:
+		found = check_github(args.username)
+		if found:
+			print(f"Username '{args.username}' found on Github.")
+		else:
+			print(f"Username '{args.username}' not found on Github.")
+	else:
+		print("Use --username to search for Github user.")
+ 
+ 
+ 
+main()
